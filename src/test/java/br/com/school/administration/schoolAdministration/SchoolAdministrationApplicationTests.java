f63e69c30bc029e44ab59f416750700ea3adc044
André Fajardo <117539643+andrefajardo@users.noreply.github.com>
2024-03-11 09:16:26 -0300

    first commit

---
+++ src/test/java/br/com/school/administration/schoolAdministration/SchoolAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.school.administration.schoolAdministration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
